c19bb066a4e89769823f76454b78ed1c1978e763
giovanni <giacoletti92@gmail.com>
2022-04-07 13:57:43 +0200

    Add images to character names add images url domain in next.config.js to allow display center characters list in the middle of the screen

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["rickandmortyapi.com"]  // necessary to allow images display
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
